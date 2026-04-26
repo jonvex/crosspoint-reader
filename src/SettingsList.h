20c0955a1b368800b39afd82e4e0259e608f7c2f
Jonathan Vexler <jbvexler@gmail.com>
2026-04-25 21:18:58 -0400

    fix spacing issue with status bar

--- src/SettingsList.h
+++ src/SettingsList.h
@@ -139,6 +139,8 @@ inline std::vector<SettingInfo> getSettingsList(const SdCardFontRegistry* regist
                            StrId::STR_CAT_READER),
         SettingInfo::Value(StrId::STR_SCREEN_MARGIN, &CrossPointSettings::screenMargin, {0, 10, 1}, "screenMargin",
                            StrId::STR_CAT_READER),
+        SettingInfo::Value(StrId::STR_STATUS_BAR_MARGIN, &CrossPointSettings::statusBarMargin, {0, 15, 1},
+                           "statusBarMargin", StrId::STR_CAT_READER),
         SettingInfo::Enum(StrId::STR_PARA_ALIGNMENT, &CrossPointSettings::paragraphAlignment,
                           {StrId::STR_JUSTIFY, StrId::STR_ALIGN_LEFT, StrId::STR_CENTER, StrId::STR_ALIGN_RIGHT,
                            StrId::STR_BOOK_S_STYLE},
